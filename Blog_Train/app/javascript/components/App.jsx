23cb214d4cd4b7bd08cc69636aa1c06f97a7167d
Nguyễn Duy Hiếu <nguyenduyhieu@MacBook-Pro-cua-Nguyen.local>
2023-08-16 11:04:24 +0700

    save

--- Blog_Train/app/javascript/components/App.jsx
+++ Blog_Train/app/javascript/components/App.jsx
@@ -4,7 +4,6 @@ import React, { useState } from 'react';
 const App = () => {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
-
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log('Name:', name);
